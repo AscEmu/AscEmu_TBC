da4254bdb389171cf9fc1ca86da875e095ce8299
Zyres <mr@eichbuehlerhof.de>
2015-11-11 12:53:16 +0100

    Added RandomUInt (start, end) function to MersenneTwister.

--- src/shared/MersenneTwister.cpp
+++ src/shared/MersenneTwister.cpp
@@ -86,10 +86,28 @@ uint32 RandomUInt(uint32 n)
     counter = 0;
     for (;;)
     {
-        c = counter%NUMBER_OF_GENERATORS;
+        c = counter % NUMBER_OF_GENERATORS;
         if (m_locks[c]->AttemptAcquire())
         {
             ret = m_generators[c]->IRandom(0, n);
+            m_locks[c]->Release();
+            return ret;
+        }
+
+        ++counter;
+    }
+}
+
+uint32 RandomUInt(uint32 n1, uint32 n2)
+{
+    uint32 ret;
+    uint32 c;
+    for (;;)
+    {
+        c = counter % NUMBER_OF_GENERATORS;
+        if (m_locks[c]->AttemptAcquire())
+        {
+            ret = m_generators[c]->IRandom(n1, n2);
             m_locks[c]->Release();
             return ret;
         }
