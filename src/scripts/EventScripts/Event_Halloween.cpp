8e6d8e6ea8db5d8095fd5c7c1d27e6b09970effc
Shoxxo <Shoxxo@exploit.im>
2015-11-12 10:59:10 +0100

    Added EventWinterVeil in EventScripts

--- src/scripts/EventScripts/Event_Halloween.cpp
+++ src/scripts/EventScripts/Event_Halloween.cpp
@@ -1,6 +1,6 @@
 /*
- * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org>
+ * ArcScripts2 Scripts for Arcemu MMORPG Server
+ * Copyright (C) 2011-2012 ArcScripts2 team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,17 +9,19 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "StdAfx.h"
-#ifndef EVENT_SCRIPTS_SETUP_H
-#define EVENT_SCRIPTS_SETUP_H
+#include "Setup.h"
 
-void SetupWinterVeil(ScriptMgr* mgr);
+/*
+Event : Halloween
+Author: Angelis
+Team  : Sun++
+*/
 
-#endif  //EVENT_SCRIPTS_SETUP_H
+//Black Cat
