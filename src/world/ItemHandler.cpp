64993728b8cee1e3e790dd5ea95804fe9c9b7da6
Tulba <matzetulba@aol.com>
2016-03-06 19:22:35 +0100

    Changed/Vendore show only useable items. (gms can see all items)

--- src/world/ItemHandler.cpp
+++ src/world/ItemHandler.cpp
@@ -1470,10 +1470,18 @@ void WorldSession::SendInventoryList(Creature* unit)
         {
             if ((curItem = ItemPrototypeStorage.LookupEntry(itr->itemid)) != 0)
             {
-                if (curItem->AllowableClass && !(_player->getClassMask() & curItem->AllowableClass) && !_player->GetSession()->HasGMPermissions()) // cebernic: GM looking up for everything.
-                    continue;
-                if (curItem->AllowableRace && !(_player->getRaceMask() & curItem->AllowableRace) && !_player->GetSession()->HasGMPermissions())
-                    continue;
+                if (!_player->HasFlag(PLAYER_FLAGS, PLAYER_FLAG_GM)) // looking up everything for active gms
+                {
+                    if (curItem->AllowableClass && !(_player->getClassMask() & curItem->AllowableClass))
+                        continue;
+
+                    if (curItem->AllowableRace && !(_player->getRaceMask() & curItem->AllowableRace))
+                        continue;
+
+                    int8 Slot = _player->GetItemInterface()->GetItemSlotByType(curItem->InventoryType);
+                    if (_player->GetItemInterface()->CanEquipItemInSlot(INVENTORY_SLOT_NOT_SET, Slot, curItem, true, true))
+                        continue;
+                }
 
                 int32 av_am = (itr->max_amount > 0) ? itr->available_amount : -1;
                 data << (counter + 1);
