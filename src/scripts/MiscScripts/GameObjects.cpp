047bba56625c0bcd06f0b9901f10ffd40cb46c52
cronicman <wolcronic@hotmail.com>
2016-08-04 20:32:25 -0400

    Core/Scripts: Removed unneeded QuelDanasPortal file and moved the code to GameObjects scripts.

--- src/scripts/MiscScripts/GameObjects.cpp
+++ src/scripts/MiscScripts/GameObjects.cpp
@@ -20,6 +20,29 @@
 #include "StdAfx.h"
 #include "Setup.h"
 
+class PortalEffectSunwellIsle : public GameObjectAIScript
+{
+public:
+	PortalEffectSunwellIsle(GameObject* goinstance) : GameObjectAIScript(goinstance) {}
+	static GameObjectAIScript *Create(GameObject * GO) { return new PortalEffectSunwellIsle(GO); }
+
+	void OnActivate(Player * pPlayer)
+	{
+		if (pPlayer->getLevel() >= 70 && pPlayer->HasSpell(44934)) // optional you can check for quest 11513
+		{
+			pPlayer->SafeTeleport(530, 530, 13001.9f, -6907.46f, 9.56139f, 0.616532f);
+		}
+		else if (pPlayer->getLevel() <= 69)
+		{
+			pPlayer->BroadcastMessage("You must be level 70 to use this!");
+		}
+		else if (!pPlayer->HasSpell(44934))
+		{
+			pPlayer->BroadcastMessage("You must complete the quest first!");
+		}
+	}
+};
+
 class OrbOfCommand : public GameObjectAIScript
 {
 public:
@@ -534,6 +557,7 @@ public:
 
 void SetupGoHandlers(ScriptMgr * mgr)
 {
+	mgr->register_gameobject_script(187056, &PortalEffectSunwellIsle::Create);
 	mgr->register_gameobject_script(179879, &OrbOfCommand::Create);
 	mgr->register_gameobject_script(184503, &OrbOfTransLocUC::Create);
 	mgr->register_gameobject_script(184502, &OrbOfTransLocSILVM::Create);
--- src/scripts/MiscScripts/Setup.h
+++ src/scripts/MiscScripts/Setup.h
@@ -22,7 +22,6 @@
 #define MISC_SCRIPTS_SETUP_H
 
 void SetupGoHandlers(ScriptMgr * mgr);
-void SetupQDGoHandlers(ScriptMgr * mgr);
 void SetupRandomScripts(ScriptMgr * mgr);
 void SetupMiscCreatures(ScriptMgr * mgr);
 
